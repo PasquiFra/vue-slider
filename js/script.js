0e4302eed9d461c9e3331adbab0667117b81cba9
Your Name <you@example.com>
2024-05-16 13:24:06 +0200

    fixes to carousel, toggle carousel improved

--- js/script.js
+++ js/script.js
@@ -1,41 +1,67 @@
-console.log("Vue OK", Vue);
+const { createApp } = Vue;
 
-const {createApp} = Vue;
-
-const app = createApp ({
+const app = createApp({
     name: "Carosello",
     data() {
         return {
             pics,
             picIndex: 0,
+            isPlaying: true,
+            autoplayButton: 'STOP AUTOPLAY',
+            autoplayInterval: null
         }
     },
     computed: {
-        lastPic (){
+        lastPic() {
             return this.pics.length - 1;
         },
-        isLastPic () {
+        isLastPic() {
             return this.picIndex === this.lastPic;
         },
-        isFirstPic () {
+        isFirstPic() {
             return this.picIndex === 0;
         }
     },
     methods: {
         changePic(target) {
-            if (target === "next"){
-                if(this.isLastPic) this.picIndex = 0
+            if (target === "next") {
+                this.stopAutoplay()
+                this.startAutoplay()
+                if (this.isLastPic) this.picIndex = 0
                 else this.picIndex++;
             } else if (target === "prev") {
-                if(this.isFirstPic) this.picIndex = this.lastPic
+                this.stopAutoplay()
+                this.startAutoplay()
+                if (this.isFirstPic) this.picIndex = this.lastPic
                 else this.picIndex--;
             } else {
                 this.picIndex = target
+                this.stopAutoplay()
             }
-        }
+        },
+        autoplay() {
+            this.autoplayInterval = setInterval(() => {
+                this.changePic('next')
+            }, 3000)
+        },
+        toggleAutoplay() {
+            this.isPlaying = !this.isPlaying;
+            this.isPlaying ? this.startAutoplay() : this.stopAutoplay();
+        },
+        startAutoplay() {
+            this.isPlaying = true;
+            this.autoplayButton = 'STOP AUTOPLAY';
+            this.autoplay();
+        },
+        stopAutoplay() {
+            this.isPlaying = false;
+            clearInterval(this.autoplayInterval);
+            this.autoplayButton = 'RESUME AUTOPLAY';
+        },
+
     },
     mounted() {
-        setInterval(() => {this.changePic("next")},3000)
+        this.autoplay();
     }
 })
 
